70d763ef009d4832f52bfa5e19cfa7ba5d0cd0a0
tomzig16 <tzigmantavicius@gmail.com>
2021-08-20 20:31:36 +0300

    Add small tests documentation

--- src/Tests/TestSuites.py
+++ src/Tests/TestSuites.py
@@ -2,7 +2,10 @@ import unittest
 
 from Tests.DummyTestCase import DummyTestCase
 
-def GetMainTestSuite():
+# Create your own function that returns a suit and then use runner.run inside RunTests to run your suit
+# If you see that your tests may be included in existing suit - include if it makes sense to couple them
+
+def GetDummyTestsSuit():
     suite = unittest.TestSuite()
     suite.addTest(DummyTestCase("test_DummyNameIsCorrect"))
     suite.addTest(DummyTestCase("test_DummyNameIsChangedCorrectly"))
@@ -10,4 +13,5 @@ def GetMainTestSuite():
 
 def RunTests():
     runner = unittest.TextTestRunner()
-    runner.run(GetMainTestSuite())
+    # runner.run(GetDummyTestsSuit()) # uncomment to run dummy tests
+
