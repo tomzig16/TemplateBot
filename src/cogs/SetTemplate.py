149a83ab0a31e474fa33ac9d2f79c18ce119dd35
KotonBads <timfrederickbinauhan@gmail.com>
2021-08-22 13:20:56 +0800

    Added functionality to main.py and changed some stuff on SetTemplate.py

--- src/cogs/SetTemplate.py
+++ src/cogs/SetTemplate.py
@@ -7,15 +7,15 @@ class SetTemplate(commands.Cog):
         self.bot = bot
     
     @commands.command(aliases = ['set_templchannel', 'set_templated_channel'])
-    async def SetTemplatedChannel(ctx, channelID: int):
+    async def SetTemplatedChannel(ctx, channelID):
         with open('TemplateChannels.json', 'r') as f:
             data = f.read()
             dictChannelID = json.loads(data)
         
-        if channelID not in dictChannelID:
+        if str(channelID) not in dictChannelID:
             with open('TemplatedChannels.json', 'w') as jsonFile:
-                json.dump(channelID, jsonFile)
+                json.dump(str(channelID), jsonFile, indent = 4)
             await ctx.send(f'<#{channelID}> added to \'Templated Channels\'')
-            
+
 def setup(bot):
     bot.add_cog(SetTemplate(bot))
--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
 import discord
 import os
 from discord.ext import commands
+
+bot = commands.Bot(command_prefix = '!')
+
+for filename in os.listdir('src/cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+bot.run(os.getenv('BOT_TOKEN'))
