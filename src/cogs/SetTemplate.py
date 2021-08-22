9ddcd80b2157f287fa5c04e2f7e944ecd64ca46f
KotonBads <timfrederickbinauhan@gmail.com>
2021-08-22 21:06:14 +0800

    (Doesn't work) add channel IDs per guild in a json file

--- src/cogs/SetTemplate.py
+++ src/cogs/SetTemplate.py
@@ -7,15 +7,16 @@ class SetTemplate(commands.Cog):
         self.bot = bot
     
     @commands.command(aliases = ['set_templchannel', 'set_templated_channel'])
-    async def SetTemplatedChannel(ctx, channelID):
-        with open('TemplateChannels.json', 'r') as f:
-            data = f.read()
-            dictChannelID = json.loads(data)
+    async def SetTemplatedChannel(self, ctx, channelID):
+        with open('src/cogs/TemplatedChannels.json', 'r') as f:
+            dictChannelID = json.load(f)
         
+        #! Doesn't work, adds the channel ID to the end of the file
         if str(channelID) not in dictChannelID:
-            with open('TemplatedChannels.json', 'w') as jsonFile:
-                json.dump(str(channelID), jsonFile, indent = 4)
-            await ctx.send(f'<#{channelID}> added to \'Templated Channels\'')
+            with open('src/cogs/TemplatedChannel.json', 'w') as f:
+                data = {f'{ctx.message.guild}':f'{channelID}'}
+                json.dump(data, f, indent=4)
+                await ctx.send(f'<#{channelID}> added to \'Templated Channels\'')
 
 def setup(bot):
     bot.add_cog(SetTemplate(bot))
