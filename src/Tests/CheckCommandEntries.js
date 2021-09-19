c0f0679b2bc257780947c10abab1159cda340848
tomzig16 <tzigmantavicius@gmail.com>
2021-09-19 13:17:12 +0300

    Rewrite CheckCommandEntries.js

--- src/Tests/CheckCommandEntries.js
+++ src/Tests/CheckCommandEntries.js
@@ -1,74 +1,50 @@
 const stringConstants = require("../Data/StringConstants");
-const commandInfoJson = require(stringConstants.paths["commandInfoPath"]);
 const fs = require("fs");
 const bot = require("../bot");
+
+const commandInfoJson = stringConstants.readCommandsInfoJson;
 bot.execute(false);
-const commandInfo = bot.commandInfo;
-var isCommandEntriesCorrectBol = true;
-var commandNameExists = true;
-var commandDescriptionExists = true;
+const botsCommandInfo = bot.commandInfo;
 
-// Getting all js filenames from /SlashFiles
-let slashFiles = fs
-    .readdirSync(stringConstants.paths["slashFilesPath"])
-    .filter((file) => file.endsWith(".js"));
+const requiredFields = ["name", "description"];
 
-let lowerCaseSlashFiles = [];
+let commandNamesFromFiles = [];
 
-// converting all filenames to lower case
-for (j in slashFiles) {
-    lowerCaseSlashFiles.push(String(slashFiles[j]).toLowerCase());
-}
+function setCommandNamesFromFiles() {
+    let slashFiles = fs
+        .readdirSync(stringConstants.paths["slashFilesPath"])
+        .filter((file) => file.endsWith(".js"));
 
-// Checking if command name and description is same as json command name and description
-for (i in lowerCaseSlashFiles) {
-    try {
-        if (
-            commandInfoJson[lowerCaseSlashFiles[i].split(".")[0]]["name"] ===
-                commandInfo[i]["name"] &&
-            commandInfoJson[lowerCaseSlashFiles[i].split(".")[0]][
-                "description"
-            ] === commandInfo[i]["description"]
-        ) {
-            isCommandEntriesCorrectBol = true;
-        } else {
-            throw "Name or description inside the command is not the same as in global JSON file";
-        }
-    } catch (e) {
-        isCommandEntriesCorrectBol = false;
-    }
-}
-
-// Check if command description exists in commandInfo json file
-for (j in lowerCaseSlashFiles) {
-    // try{
-    try {
-        if (
-            commandInfoJson[lowerCaseSlashFiles[j].split(".")[0]][
-                "description"
-            ] === undefined
-        ) {
-            commandDescriptionExists = false;
-        }
-    } catch (e) {
-        commandDescriptionExists = false;
-    }
-}
-
-for (j in lowerCaseSlashFiles) {
-    try {
-        if (
-            commandInfoJson[lowerCaseSlashFiles[j].split(".")[0]]["name"] ===
-            undefined
-        ) {
-        }
-    } catch (e) {
-        commandNameExists = false;
+    let lowerCaseSlashFiles = [];
+    for (let i in slashFiles) {
+        lowerCaseSlashFiles.push(String(slashFiles[i]).toLowerCase());
+        commandNamesFromFiles.push(lowerCaseSlashFiles[j].split(".")[0]);
     }
 }
 
 module.exports = {
-    commandEntriesCorrect: isCommandEntriesCorrectBol,
-    commandNameExists: commandNameExists,
-    commandDescriptionExists: commandDescriptionExists,
+    setupTestsData: function() {
+        setCommandNamesFromFiles();
+    },
+
+    isCommandPropertiesSameinJSON: function() {
+        let areEntriesCorrect = true;
+        for (let commandName in commandNamesFromFiles) {
+            if (commandInfoJson[commandName]["name"] !== commandInfo[commandName]["name"] ||
+                commandInfoJson[commandName]["description"] !== commandInfo[commandName]["description"] ) {
+                return false;
+            }
+        }
+        return true;
+    },
+    doesJSONContainAllRequiredFields: function() {    
+        for (let commandName of commandNamesFromFiles) {
+            for(let requiredField of requiredFields) {
+                if (commandInfoJson[commandName][requiredField] === undefined) {
+                    return false;
+                }
+            }        
+        }
+        return true;
+    }
 };
