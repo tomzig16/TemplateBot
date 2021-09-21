efa4811aa0f41b890e449352842bbc674485c1c2
Prashant <prashantbaid80@gmail.com>
2021-09-21 19:16:25 +0530

    fix conflicts

--- src/EventFiles/OnReady.js
+++ src/EventFiles/OnReady.js
@@ -1,9 +1,9 @@
-const logger = require("../LoggerFiles/InitializeLogger.js");
+const {loggerInstance} = require("../LoggerFiles/Logger.js");
 
 module.exports = {
     name: "ready",
     once: true,
     execute(client) {
-        logger.log(logger.LogLevelInfo, "Bot has logged in successfully");
+        loggerInstance.log(loggerInstance.LogLevelInfo, "Bot has logged in successfully");
     },
 };
--- src/LoggerFiles/InitializeLogger.js
+++
@@ -1,6 +0,0 @@
-const logger = require("./Logger.js");
-
-const Logger = new logger("DEV");
-
-// Export the variable
-module.exports = Logger;
